63b7440bcb7474bcb65729acdd38df466f68caa0
Daniel Wagner <wagi@kernel.org>
2026-03-07 19:52:39 +0100

    build-all: add a dummy muon/samu
    In order to test build all containers, a dummy muan/samu file
    needs to exists.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- build-all.sh
+++ build-all.sh
@@ -5,6 +5,18 @@
 #
 # Author: Daniel Wagner <dwagner@suse.de>
 
+mkdir -p bin
+
+cat > bin/samu <<'EOF'
+#!/usr/bin/env sh
+echo "dummy samu"
+EOF
+
+cat > bin/muon <<'EOF'
+#!/usr/bin/env sh
+echo "dummy muon"
+EOF
+
 for dockerfile in main/Dockerfile.*; do
     docker buildx build --platform linux/amd64 . -f "${dockerfile}"
 done
